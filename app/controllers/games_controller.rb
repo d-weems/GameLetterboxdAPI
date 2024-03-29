f2a66b1643f2b2c8a50e40c1275782f0d603c5f0
Darien Weems <dweems38@gmail.com>
2024-03-29 13:29:55 -0400

    Added controllers for all models, built game controller skeleton

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,43 @@
+class GamesController < ApplicationController
+  before_action :set_game, only: [:show, :update, :destroy]
+
+  def index
+    @games = Game.all
+    render json: @games
+  end
+
+  def show
+    render json: @game
+  end
+
+  def create
+    @game = Game.new(game_params)
+
+    if @game.save
+      render json: @game, status: :created
+    else
+      render json: @game.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @game.update(game_params)
+      render json: @game
+    else
+      render json: @game.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @game.destroy
+  end
+
+  private
+    def set_game
+      @game = Game.find(params[:id])
+    end
+
+    def game_params
+      params.require(:game).permit(:title, :release_year)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,8 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+
+  resources :games
+  resources :users
+  resources :gamelogs
 end
